a9b0f39024436d5f8f34c0b83dfd6fab6130db38
Xudong Han <xudongh1@student.unimelb.edu.au>
2022-02-15 16:04:56 +0800

    fix minor bug

---
+++ dev/dev_main.py
@@ -0,0 +1,9 @@
+import logging
+
+import sys
+sys.path.append('../')
+
+from src.base_options import Options
+
+if __name__ == '__main__':
+    state = Options.get_state()
--- src/dataloaders/__init__.py
+++ src/dataloaders/__init__.py
@@ -5,6 +5,17 @@ from .loaders import *
 from . import utils
 from collections import defaultdict
 
+if sys.platform == "win32":
+    default_dataset_roots = dict(
+        Moji='D:\\Project\\User_gender_removal\\data\\deepmoji\\split2\\',
+        Bios='D:\Project\Minding_Imbalance_in_Discriminator_Training\data\bios'
+    )
+else:
+    default_dataset_roots = dict(
+        Moji='/data/cephfs/punim1421/Dataset/deepmoji/split2/',
+        Bios='./data/cephfs/punim1421/Dataset/bios_gender_economy'
+    )
+
 def get_dataloaders(args):
     assert args.dataset in ["Moji", "Bios"], "Not implemented"
     if args.dataset == "Moji":
