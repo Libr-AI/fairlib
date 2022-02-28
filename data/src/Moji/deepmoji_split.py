650f6eefd09ba2dba32cf23a41ae9e31541faf50
Xudong Han <xudongh1@student.unimelb.edu.au>
2022-02-28 19:20:27 +0800

    From INLP paper

---
+++ data/src/Moji/deepmoji_split.py
@@ -0,0 +1,38 @@
+"""
+Usage:
+  deepmoji_split.py [--input_dir=INPUT_DIR] [--output_dir=OUTPUT_DIR]
+
+Options:
+  -h --help                     show this help message and exit
+  --input_dir=INPUT_DIR         input dir file
+  --output_dir=OUTPUT_DIR       write down output file
+
+Script for converting the encoded deep_moji encoded vectors into train/dev/test splits
+"""
+
+import numpy as np
+# from docopt import docopt
+import os
+
+
+def read_data_file(input_file: str):
+    vecs = np.load(input_file)
+
+    np.random.shuffle(vecs)
+
+    return vecs[:40000], vecs[40000:42000], vecs[42000:44000]
+
+
+if __name__ == '__main__':
+    # arguments = docopt(__doc__)
+
+    in_dir = "D:\Project\\nullspace_projection\data\deepmoji"
+    out_dir = "D:\Project\\nullspace_projection\data\deepmoji"
+
+    os.makedirs(out_dir, exist_ok=True)
+
+    for split in ['pos_pos', 'pos_neg', 'neg_pos', 'neg_neg']:
+        train, dev, test = read_data_file(in_dir + '/' + split + '.npy')
+        for split_dir, data in zip(['train', 'dev', 'test'], [train, dev, test]):
+            os.makedirs(out_dir + '/' + split_dir, exist_ok=True)
+            np.save(out_dir + '/' + split_dir + '/' + split + '.npy', data)
