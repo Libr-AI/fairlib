18e3794ed7b63f195343071624347b5847593565
Xudong Han <xudongh1@student.unimelb.edu.au>
2022-03-01 23:43:56 +0800

    create scripts for adv and DAdv over Moji

--- scripts/hypertune/vanilla_Bios_gender_0.sh
+++
@@ -1,3 +0,0 @@
-python main.py --project_dir hypertune --dataset Bios_gender --emb_size 768 --num_classes 28 --batch_size 512 --learning_rate 0.01 --hidden_size 200 --n_hidden 1 --dropout 0.0 --base_seed 1 --exp_id hypertune_vanilla0
-sleep 2
-python main.py --project_dir hypertune --dataset Bios_gender --emb_size 768 --num_classes 28 --batch_size 512 --learning_rate 0.01 --hidden_size 200 --n_hidden 1 --dropout 0.0 --batch_norm --base_seed 1 --exp_id hypertune_vanilla1
--- scripts/hypertune/vanilla_Moji_0.sh
+++
@@ -1,3 +0,0 @@
-python main.py --project_dir hypertune --dataset Moji --emb_size 2304 --num_classes 2 --batch_size 512 --learning_rate 0.01 --hidden_size 200 --n_hidden 1 --dropout 0.0 --base_seed 1 --exp_id hypertune_vanilla3024
-sleep 2
-python main.py --project_dir hypertune --dataset Moji --emb_size 2304 --num_classes 2 --batch_size 512 --learning_rate 0.01 --hidden_size 200 --n_hidden 1 --dropout 0.0 --batch_norm --base_seed 1 --exp_id hypertune_vanilla3025
