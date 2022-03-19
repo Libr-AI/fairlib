1bbed6764905cd7046807219f8ab32cf087d7e1a
Xudong Han <xudongh1@spartan-login3.hpc.unimelb.edu.au>
2022-03-19 13:21:47 +1100

    retrive delayed cls results over moji

--- analysis/retrive_results.py
+++ analysis/retrive_results.py
@@ -2,43 +2,15 @@ from src.load_results import model_selection_parallel as model_selection
 
 
 FairSCL_df = model_selection(
-    results_dir= r"/data/cephfs/punim0478/xudongh1/experimental_results/Fair_NLP_Classification",
-    project_dir= r"FairSCL/Bios_gender",
+    results_dir= r"/data/cephfs/punim1421/Fair_NLP_Classification/results",
+    project_dir= r"hypertune/Moji",
     checkpoint_dir= "models",
     checkpoint_name= "checkpoint_epoch",
-    model_id= ("FSCL"),
+    model_id= ("DelayedCLS_Adv"),
     GAP_metric_name = "TPR_GAP",
     Performance_metric_name = "accuracy",
     selection_criterion = "DTO",
     n_jobs=20,
-    index_column_names = ["fcl_lambda_y"],
-    save_path = "results/Bios_gender_FairSCL_df.pkl"
-    )
-
-GDEO_df = model_selection(
-    results_dir= r"/data/cephfs/punim0478/xudongh1/experimental_results/Fair_NLP_Classification",
-    project_dir= r"GroupDifference/Bios_gender",
-    checkpoint_dir= "models",
-    checkpoint_name= "checkpoint_epoch",
-    model_id= ("GDEO"),
-    GAP_metric_name = "TPR_GAP",
-    Performance_metric_name = "accuracy",
-    selection_criterion = "DTO",
-    n_jobs=20,
-    index_column_names = ["DyBTalpha"],
-    save_path = "results/Bios_gender_GDEO_df.pkl"
-    )
-
-GDMean_df = model_selection(
-    results_dir= r"/data/cephfs/punim0478/xudongh1/experimental_results/Fair_NLP_Classification",
-    project_dir= r"GroupDifference/Bios_gender",
-    checkpoint_dir= "models",
-    checkpoint_name= "checkpoint_epoch",
-    model_id= ("GDMean"),
-    GAP_metric_name = "TPR_GAP",
-    Performance_metric_name = "accuracy",
-    selection_criterion = "DTO",
-    n_jobs=20,
-    index_column_names = ["DyBTalpha"],
-    save_path = "results/Bios_gender_GDMean_df.pkl"
+    index_column_names = ["classification_head_update_frequency", "adv_lambda"],
+    save_path = "results/Moji_DelayedCLS_Adv_df.pkl"
     )
