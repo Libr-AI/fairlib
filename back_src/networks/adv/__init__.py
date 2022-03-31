00098248f54fca39694e99220162007b6c0f5f3e
Xudong Han <xudongh1@student.unimelb.edu.au>
2022-03-31 20:04:29 +0800

    Remove duplicated results

--- back_src/networks/adv/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-import torch
-import logging
-from .discriminator import Discriminator
-from . import utils
-from collections import defaultdict
-from . import customized_loss
--- back_src/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .utils import *
-from . import logging
