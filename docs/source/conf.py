fa0a9ab87a2bf851e9cd11bc403c6ae9dbfd36e0
Xudong Han <xudongh1@student.unimelb.edu.au>
2022-05-10 23:34:16 +0800

    remove sourcelink from each page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Xudong Han'
 author = 'Xudong Han'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = ''
 
 
 # -- General configuration ---------------------------------------------------
@@ -54,8 +54,14 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+# don't show the "view source" link
+html_show_sourcelink = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'friendly'  # igor is also good
+
