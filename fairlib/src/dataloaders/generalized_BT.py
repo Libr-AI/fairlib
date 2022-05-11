f24614debea28d315a2475b7978e92423838e732
Xudong Han <xudongh1@student.unimelb.edu.au>
2022-05-11 19:49:19 +0800

    sampling function for different situation

--- fairlib/src/dataloaders/generalized_BT.py
+++ fairlib/src/dataloaders/generalized_BT.py
@@ -1,8 +1,9 @@
 import itertools
 import numpy as np
+from random import choices
 
 def get_data_distribution(y_data, g_data):
-    """Given target label and protected labels, calculate emprical distributions.
+    """Given target label and protected labels, calculate empirical distributions.
 
     joint_dist: n_class * n_groups matrix, where each element refers to the joint probability, i.e., proportion size.
     g_dist: n_groups array, indicating the prob of each group
@@ -63,3 +64,90 @@ def get_data_distribution(y_data, g_data):
     }
 
     return distribution_dict
+
+def generalized_sampling(
+    default_distribution_dict,
+    N=None, joint_dist=None, g_dist=None, y_dist=None, 
+    g_cond_y_dist=None,y_cond_g_dist=None):
+    """Perform resampling according to the specified distribution information
+
+    Args:
+        default_distribution_dict (dict): a dict of distribution information of the original dataset.
+        N (int, optional): The total number of returned indices. Defaults to None.
+        joint_dist (np.ndarray, optional): n_class * n_groups matrix, where each element refers to the joint probability, i.e., proportion size. Defaults to None.
+        g_dist (np.ndarray, optional): n_groups array, indicating the prob of each group. Defaults to None.
+        y_dist (np.ndarray, optional): n_class array, indicating the prob of each class. Defaults to None.
+        g_cond_y_dist (np.ndarray, optional): n_class * n_groups matrix, g_cond_y_dit[y_id,:] refers to the group distribution within class y_id. Defaults to None.
+        y_cond_g_dist (np.ndarray, optional): n_class * n_groups matrix, y_cond_g_dit[:,g_id] refers to the class distribution within group g_id. Defaults to None.
+
+    Returns:
+        list: list of selected indices.
+    """
+
+    if joint_dist is not None:
+        assert joint_dist.shape == default_distribution_dict["joint_dist"].shape
+        target_joint_dist = joint_dist
+    else:
+        target_joint_dist = default_distribution_dict["joint_dist"]
+
+    if g_dist is not None:
+        assert g_dist.shape == default_distribution_dict["g_dist"].shape
+        target_g_dist = g_dist
+    else:
+        target_g_dist = default_distribution_dict["g_dist"]
+
+    if y_dist is not None:
+        assert y_dist.shape == default_distribution_dict["y_dist"].shape
+        target_y_dist = y_dist
+    else:
+        target_y_dist = default_distribution_dict["y_dist"]
+
+    if g_cond_y_dist is not None:
+        assert g_cond_y_dist.shape == default_distribution_dict["g_cond_y_dist"].shape
+        target_g_cond_y_dist = g_cond_y_dist
+    else:
+        target_g_cond_y_dist = default_distribution_dict["g_cond_y_dist"]
+
+    if y_cond_g_dist is not None:
+        assert y_cond_g_dist.shape == default_distribution_dict["y_cond_g_dist"].shape
+        target_y_cond_g_dist = y_cond_g_dist
+    else:
+        target_y_cond_g_dist = default_distribution_dict["y_cond_g_dist"]
+
+    # 1st priority: joint_dict
+    if joint_dist is not None:
+        target_dist = joint_dist
+    
+    # 2nd priority: y_dist and y_cond_g_dist
+    elif (y_dist is not None) or (y_cond_g_dist is not None):
+        target_dist = target_g_cond_y_dist * target_y_dist.reshape(-1,1)
+    
+    # 3rd priority: g_dist and g_cond_y_dist
+    elif (g_dist is not None) or (g_cond_y_dist is not None):
+        target_dist = target_y_cond_g_dist * target_g_dist.reshape(1,-1)
+
+    # If all arguments are None, use the original distribution for sampling
+    else:
+        target_dist = default_distribution_dict["joint_dist"]
+    
+    # Make sure that the sum of probs is 1
+    target_dist = target_dist / np.sum(target_dist)
+
+    # Calculate the number of instacnes for each subset
+    if N is not None:
+        target_dist = target_dist * int(N)
+    else:
+        target_dist = target_dist * default_distribution_dict["N"]
+    
+    selected_index = []
+    # Sampling
+    for _yg_tuple, _yg_index in default_distribution_dict["yg_index"].items():
+        _target_N = int(round(target_dist[_yg_tuple]))
+
+        if _target_N <= len(_yg_index):
+            selected_index = selected_index + list(_yg_index[:_target_N])
+        else:
+            selected_index = selected_index + list(_yg_index)
+            selected_index = selected_index + choices(_yg_index, k=(_target_N - len(_yg_index)))
+    
+    return selected_index
