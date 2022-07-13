ecaac8bf6ab00b103b44ca8585dd58a329de4bfa
Xudong Han <xudongh1@student.unimelb.edu.au>
2022-07-13 16:37:06 +0800

    dataset API for moji

---
+++ fairlib/datasets/__init__.py
@@ -0,0 +1,12 @@
+from . import moji
+
+name2class = {
+    "Moji":moji,
+    "moji":moji,
+}
+
+def prepare_dataset(name, dest_folder):
+    if name in name2class.keys():
+        data_class = name2class[name]
+        initialized_class = data_class.init_data_class(dest_folder=dest_folder)
+        initialized_class.prepare_data()
