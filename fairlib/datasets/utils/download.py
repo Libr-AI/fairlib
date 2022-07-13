b0888508a472c54a681b49486339ba48d5be723f
Xudong Han <xudongh1@student.unimelb.edu.au>
2022-07-13 15:42:47 +0800

    Create download.py
    download files

---
+++ fairlib/datasets/utils/download.py
@@ -0,0 +1,24 @@
+import os
+import requests
+from tqdm.auto import tqdm
+
+def download(url: str, dest_folder: str, chunk_size: int = 1024*8):
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)  # create folder if it does not exist
+
+    filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
+    file_path = os.path.join(dest_folder, filename)
+
+    r = requests.get(url, stream=True)
+    total_length = int(r.headers.get('content-length'))
+
+    if r.ok:
+        print("saving to", os.path.abspath(file_path))
+        with open(file_path, 'wb') as f:
+            for chunk in tqdm(r.iter_content(chunk_size=chunk_size), total=(total_length//chunk_size)+1):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+    else:  # HTTP status code 4XX/5XX
+        print("Download failed: status code {}\n{}".format(r.status_code, r.text))
