41e15aabc568cf91c6a736ee980ec4297c8f9aa3
Xudong Han <xudongh1@student.unimelb.edu.au>
2022-07-18 11:21:41 +0800

    normalized dataset names

--- fairlib/datasets/__init__.py
+++ fairlib/datasets/__init__.py
@@ -2,20 +2,18 @@ from . import moji
 from . import bios
 from . import coloredMNIST
 from . import COMPAS
+from . import TP_POS
 
 name2class = {
-    "Moji":moji,
     "moji":moji,
     "bios":bios,
-    "Bios":bios,
-    "coloredMNIST":coloredMNIST,
-    "MNIST":coloredMNIST,
-    "COMPAS":COMPAS,
+    "coloredmnist":coloredMNIST,
     "compas":COMPAS,
+    "tp_pos":TP_POS
 }
 
 def prepare_dataset(name, dest_folder,batch_size=64):
     if name in name2class.keys():
-        data_class = name2class[name]
+        data_class = name2class[name.lower()]
         initialized_class = data_class.init_data_class(dest_folder=dest_folder, batch_size=batch_size)
         initialized_class.prepare_data()
