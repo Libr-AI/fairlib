0fd78797191563356a42c64e2ec9525803c632ec
Xudong Han <xudongh1@student.unimelb.edu.au>
2022-11-26 10:05:09 +0800

    support different criteria for early stopping

--- fairlib/src/evaluators/__init__.py
+++ fairlib/src/evaluators/__init__.py
@@ -57,3 +57,45 @@ def present_evaluation_scores(
     logging.info(('Validation {}').format("".join(validation_results)))
     Test_results = ["{}: {:2.2f}\t".format(k, 100.*test_scores[k]) for k in test_scores.keys()]
     logging.info(('Test {}').format("".join(Test_results)))
+
+
+def validation_is_best(
+    valid_preds, valid_labels, valid_private_labels,
+    model, epoch_valid_loss, selection_criterion = "DTO",
+    performance_metric = "accuracy", fairness_metric="TPR_GAP"
+    ):
+    """
+    Check is the current model is the best so far.
+    """
+
+    is_best = False
+
+    valid_scores, _ = gap_eval_scores(
+        y_pred=valid_preds,
+        y_true=valid_labels,
+        protected_attribute=valid_private_labels,
+        args = model.args,
+        )
+
+    if selection_criterion == "DTO":
+        valid_dto_score = ((1-valid_scores[performance_metric])**2 + valid_scores[fairness_metric]**2)**0.5
+        if valid_dto_score < model.best_valid_loss:
+            model.best_valid_loss = valid_dto_score
+            is_best = True
+    elif selection_criterion == "Loss":
+        if epoch_valid_loss < model.best_valid_loss:
+            model.best_valid_loss = epoch_valid_loss
+            is_best = True
+    elif selection_criterion == "Performance":
+        if (1-valid_scores[performance_metric]) < model.best_valid_loss:
+            model.best_valid_loss = 1-valid_scores[performance_metric]
+            is_best = True
+    elif selection_criterion == "Fairness":
+        if valid_scores[fairness_metric] < model.best_valid_loss:
+            model.best_valid_loss = valid_scores[fairness_metric]
+            is_best = True
+    else:
+        raise NotImplementedError
+
+
+    return is_best
